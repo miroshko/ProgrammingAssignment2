647be54a5efa42a5e54e6e20834fba8f60f933f1
Oleksii Miroshko <miroshko@live.com>
2015-05-24 12:09:48 +0200

    caching implemented, sample usage added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## prepares a matrix for being passed to the cacheSolve function
+## the function returns a list which is a storage for the original matrix and the result of the inversion
+## the list contains 4 functions:
+## set
+## get
+## getSolved
+## setSolved
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        
+        setSolved <- function(solved) s <<- solved
+        getSolved <- function() s
+        
+        list( set = set,
+              get = get,
+              setSolved = setSolved,
+              getSolved = getSolved )
 }
 
 
-## Write a short comment describing this function
+## returns result of the solve function and stores it in the internal cache for immediate returning
+## in case of further calls with the same arguments 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getSolved()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setSolved(m)
+        m
 }
+
+# Aample usage:
+# 
+# creating a 1000 x 1000 random matrix.
+# (There is though a slight chance that a random matrix is irrevertible, in this case just repeat) 
+sampleMatrix <- matrix(runif(1000000, 0, 100), 1000, 1000)
+#
+# preparing the matrix for cacheSolve
+sampleCacheMatrix = makeCacheMatrix(sampleMatrix)
+#
+# perform inversion on the same data 2 times and measure time of both calls
+# should take long
+ptm <- proc.time()
+solved <- cacheSolve(sampleCacheMatrix)
+diff <- proc.time() - ptm
+
+print(diff['elapsed'])
+
+# should take less
+ptm <- proc.time()
+solved <- cacheSolve(sampleCacheMatrix)
+diff <- proc.time() - ptm
+
+print(diff['elapsed'])
