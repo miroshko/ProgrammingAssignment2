92b54fe2566d0114d383eb34f376f4d225329ac7
Oleksii Miroshko <miroshko@live.com>
2015-05-24 12:32:17 +0200

    added comments to the code

--- cachematrix.R
+++ cachematrix.R
@@ -7,18 +7,27 @@
 ## setSolved
 
 makeCacheMatrix <- function(x = matrix()) {
-        s <- NULL
-        set <- function(y) {
+        # x is added to the lexical scope of the function and is available
+        # for all function which are defined inside of makeCacheMatrix
+        
+        # initializing
+        solution <- NULL
+
+        # set a new matrix to the existing data structure
+        setMatrix <- function(y) {
+                # with <<- we access the x from the upper scope (makeCacheMatrix)
                 x <<- y
-                s <<- NULL
+                
+                # clear the solution for the new matrix
+                solution <<- NULL
         }
-        get <- function() x
+        getMatrix <- function() x
         
-        setSolved <- function(solved) s <<- solved
-        getSolved <- function() s
+        setSolved <- function(solved) solution <<- solved
+        getSolved <- function() solution
         
-        list( set = set,
-              get = get,
+        list( setMatrix = setMatrix,
+              getMatrix = getMatrix,
               setSolved = setSolved,
               getSolved = getSolved )
 }
@@ -28,37 +37,39 @@ makeCacheMatrix <- function(x = matrix()) {
 ## in case of further calls with the same arguments 
 
 cacheSolve <- function(x, ...) {
+        # if solution is present return the result immediately,
+        # otherwise perform calculation, save the solution and return the result
         m <- x$getSolved()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
-        data <- x$get()
+        data <- x$getMatrix()
         m <- solve(data, ...)
         x$setSolved(m)
         m
 }
 
-## Sample usage:
-## 
-## creating a 1000 x 1000 random matrix.
-## (There is though a slight chance that a random matrix is irrevertible, in this case just repeat) 
-#sampleMatrix <- matrix(runif(1000000, 0, 100), 1000, 1000)
-##
-## preparing the matrix for cacheSolve
-#sampleCacheMatrix = makeCacheMatrix(sampleMatrix)
-##
-## perform inversion on the same data 2 times and measure time of both calls
-## should take long, ~ 8 sec on my PC
-#ptm <- proc.time()
-#solved <- cacheSolve(sampleCacheMatrix)
-#diff <- proc.time() - ptm
-#
-#print(diff['elapsed'])
-#
-## should take less
-#ptm <- proc.time()
-#solved <- cacheSolve(sampleCacheMatrix)
-#diff <- proc.time() - ptm
-#
-#print(diff['elapsed'])
+#  # Sample usage (uncomment and run if needed):
+#   
+#  # creating a 1000 x 1000 random matrix.
+#  # (There is though a slight chance that a random matrix is irrevertible, in this case just repeat) 
+#  sampleMatrix <- matrix(runif(1000000, 0, 100), 1000, 1000)
+#  
+#  # preparing the matrix for cacheSolve
+#  sampleCacheMatrix = makeCacheMatrix(sampleMatrix)
+#  
+#  # perform inversion on the same data 2 times and measure time of both calls
+#  # should take long, ~ 8 sec on my PC
+#  ptm <- proc.time()
+#  solved <- cacheSolve(sampleCacheMatrix)
+#  diff <- proc.time() - ptm
+#  
+#  print(diff['elapsed'])
+#  
+#  # should take less
+#  ptm <- proc.time()
+#  solved <- cacheSolve(sampleCacheMatrix)
+#  diff <- proc.time() - ptm
+#  
+#  print(diff['elapsed'])
